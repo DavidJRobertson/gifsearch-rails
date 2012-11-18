c4bd42ff2c7496a7d97754b7cf196fa1f52ac022
David Robertson <david@davidr.me>
2012-11-18 21:47:05 +0000

    Lazy assets...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Gifsearch::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true #TODO: Revert to false upon first deploy
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
